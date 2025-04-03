6a97225a1f4997f9ef493debef581eff402fb60e
𝓨𝓪𝓿𝓾𝔃 𝓑𝓮𝔂 <160026656+realyavuzbey@users.noreply.github.com>
2025-04-03 20:45:08 +0300

    Update __init__.py

--- vigilante/__init__.py
+++ vigilante/__init__.py
@@ -7,6 +7,7 @@ https://vigilante.top
 
 from .core import Vigilante
 from .nightcrawler import Nightcrawler
+from .session import Session
 
 __all__ = ["Vigilante", "Nightcrawler"]
 __version__ = "0.1.0"
