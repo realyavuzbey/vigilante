6fc9dc1441b5e3feebe60df21271e1f4ad84aef8
𝓨𝓪𝓿𝓾𝔃 𝓑𝓮𝔂 <160026656+realyavuzbey@users.noreply.github.com>
2025-04-03 23:43:23 +0300

    Update __init__.py

--- vigilante/__init__.py
+++ vigilante/__init__.py
@@ -11,3 +11,6 @@ from .session import Session
 
 __all__ = ["Vigilante", "Nightcrawler", "Session"]
 __version__ = "0.1.0"
+__author__ = "Yavuz Bey"
+__license__ = "MIT"
+__website__ = "https://vigilante.top"
