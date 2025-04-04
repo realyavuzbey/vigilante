047c37e5f292f2ecee9fcba55b334290ff9d50ba
𝓨𝓪𝓿𝓾𝔃 𝓑𝓮𝔂 <160026656+realyavuzbey@users.noreply.github.com>
2025-04-04 04:00:24 +0300

    Update __init__.py

--- vigilante/__init__.py
+++ vigilante/__init__.py
@@ -7,6 +7,7 @@ https://vigilante.top
 
 from .core import Vigilante
 from .nightcrawler import Nightcrawler
+from .scraptor import Scraptor
 from .session import Session
 
 __all__ = ["Vigilante", "Nightcrawler", "Session"]
