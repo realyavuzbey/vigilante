352d1f086adaccb97ce33c9685218fce72ce3769
𝓨𝓪𝓿𝓾𝔃 𝓑𝓮𝔂 <160026656+realyavuzbey@users.noreply.github.com>
2025-04-05 00:43:03 +0300

    Update __init__.py

--- vigilante/__init__.py
+++ vigilante/__init__.py
@@ -11,7 +11,7 @@ from .scraptor import Scraptor
 from .session import Session
 
 __all__ = ["Vigilante", "Nightcrawler", "Session"]
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "Yavuz Bey"
 __license__ = "MIT"
 __website__ = "https://vigilante.top"
