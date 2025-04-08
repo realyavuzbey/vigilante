6fd3c55d5bd8435dcabbdc019f0e8d34f0c6a410
Yavuz Bey <realyavuzbey@gmail.com>
2025-04-09 02:58:40 +0300

    auto: loader push + version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vigilante',
-    version='0.1.0',
+    version="0.1.1",
     description='Dark Web Recon & Intelligence CLI by @realyavuzbey',
     author='Yavuz Bey',
     url='https://github.com/realyavuzbey/vigilante',
